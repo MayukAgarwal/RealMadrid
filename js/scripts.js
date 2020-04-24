ca5c91fe0935a19d5e4a6bb95f865ae8a07d7527
Mayuk <mayukagarwal@gmail.com>
2020-04-24 10:22:28 +0530

    goBack function added - window and history used

--- js/scripts.js
+++ js/scripts.js
@@ -119,11 +119,16 @@ function runTheClock() {
     hrPosition = hrPosition + (3 / 360);
     minPosition = minPosition + (6 / 60);
     secPosition = secPosition + 6;
-
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
 }
 
 var interval = setInterval(runTheClock, 1000);
+
+var dateParent = document.getElementById('dateTime')
+var displayDate = document.createElement("div"); // Create a <div> element
+var date = new Date();
+var dateNode = document.createTextNode(date);
+displayDate.appendChild(dateNode);
+dateParent.appendChild(displayDate);
